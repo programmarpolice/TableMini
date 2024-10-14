75ee2050b1bf4f0a552cd321a64884be8d9bc9dd
Joy Harrington <joysyk23@gmail.com>
2024-10-14 11:08:42 -0500

    Complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,25 @@
 const prisma = require("../prisma");
-const seed = async () => {
+const { faker } = require("@faker-js/faker");
+const seed = async (numRestaurants = 3, numReservations = 5) => {
   // TODO: Create 3 restaurants with 5 reservations each
+  for (let i = 0; i < numRestaurants; i++) {
+    const reservations = Array.from({ length: numReservations }, (_, j) => {
+      const name = faker.internet.displayName();
+      return {
+        name,
+        email: `${i}${j}@foo.bar`,
+        partySize: faker.number.int({ min: 1, max: 10 }),
+      };
+    });
+    await prisma.restaurant.create({
+      data: {
+        name: faker.company.buzzAdjective() + " " + faker.company.buzzNoun(),
+        reservations: {
+          create: reservations,
+        },
+      },
+    });
+  }
 };
 seed()
   .then(async () => await prisma.$disconnect())
